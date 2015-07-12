ed677ac9994933d60a049043a506ceb430deaacb
Meemaw <totalfr33k@gmail.com>
2015-07-12 18:55:46 +0200

    Name fix

--- Problem_67.py
+++ Problem_67.py
@@ -2,18 +2,21 @@ __author__ = 'Meemaw'
 
 
 
-list = []
+vhodnaTabela = []
 indeks = 0
+
+
 
 with open("odpri.txt") as file:
     for line in file:
         indeks+=1
         line = line.split()
-        list.append(line)
-list.append([0]*(indeks+1))
+        vhodnaTabela.append(map(int,line))
+vhodnaTabela.append([0]*(indeks+1))
+
 
 for i in range(indeks-1,-1,-1):
-    for x in range(len(list[i])):
-        list[i][x] = max(list[i+1][x],list[i+1][x+1]) + int(list[i][x])
-print(list[0])
+    for x in range(len(vhodnaTabela[i])):
+        vhodnaTabela[i][x] += max(vhodnaTabela[i+1][x],vhodnaTabela[i+1][x+1])
+print(vhodnaTabela[0])
 
